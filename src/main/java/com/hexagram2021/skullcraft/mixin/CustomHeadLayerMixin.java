5e49b44d69b9f86673d02a5a384e56df039ae4f9
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-08-06 17:58:09 +0800

    渲染优化：佩戴头颅时不渲染玩家的脑袋

--- src/main/java/com/hexagram2021/skullcraft/mixin/CustomHeadLayerMixin.java
+++ src/main/java/com/hexagram2021/skullcraft/mixin/CustomHeadLayerMixin.java
@@ -2,6 +2,7 @@ package com.hexagram2021.skullcraft.mixin;
 
 import com.hexagram2021.skullcraft.SkullCraft;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.HeadedModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
 import net.minecraft.nbt.CompoundTag;
@@ -15,17 +16,23 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import javax.annotation.Nullable;
+
 @Mixin(CustomHeadLayer.class)
 public class CustomHeadLayerMixin<T extends LivingEntity> {
+	@Nullable
 	private CompoundTag blockItemTag = null;
 
 	@Redirect(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getTag()Lnet/minecraft/nbt/CompoundTag;"))
+	@Nullable
 	public CompoundTag getBlockItemTag(ItemStack instance) {
 		return this.blockItemTag = instance.getTag();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V", shift = At.Shift.BEFORE))
-	public void HandleSkullCraftScale(PoseStack transform, MultiBufferSource source, int uv2, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+	public void handleSkullCraftScale(PoseStack transform, MultiBufferSource source, int uv2, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+		((CustomHeadLayer<T, ? extends HeadedModel>)(Object)this).getParentModel().getHead().skipDraw = true;
 		if (this.blockItemTag != null && this.blockItemTag.contains(SkullCraft.SCALE_TAG, Tag.TAG_COMPOUND)) {
 			final CompoundTag scaleNBT = this.blockItemTag.getCompound(SkullCraft.SCALE_TAG);
 			final int scaleX = scaleNBT.contains("x") ? Mth.clamp(scaleNBT.getInt("x"), 50, 5000) : 100;
@@ -37,5 +44,11 @@ public class CustomHeadLayerMixin<T extends LivingEntity> {
 			transform.scale((float)dx, (float)dy, (float)dz);
 		}
 		this.blockItemTag = null;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Inject(method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V", at = @At(value = "HEAD"))
+	public void resetSkipDraw(PoseStack transform, MultiBufferSource source, int uv2, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+		((CustomHeadLayer<T, ? extends HeadedModel>)(Object)this).getParentModel().getHead().skipDraw = false;
 	}
 }
